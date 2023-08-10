5a2cbaabed21321829dc52fba8e0b749c4ad1c6c
Simon Nganga <ngangasimoncs@gmail.com>
2023-08-10 14:14:46 -0700

    a C program that prints exact message - printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *
+ * main - entry point
+ *
+ * Description: Write a C program that prints exactly with proper grammar, but the outcome is a piece of art
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
