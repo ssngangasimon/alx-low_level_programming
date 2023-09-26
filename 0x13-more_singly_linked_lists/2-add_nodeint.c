1d8038237789bac21793a267ff86ec8fc96670ff
Simon Nganga <ngangasimoncs@gmail.com>
2023-09-26 12:09:29 -0700

    changes

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - function that adds a new node at the beginning of a listint_t list
+ * add_nodeint - function that adds new node at beginning of listint_t list
  * @head: to first node
  * @n: data
  *
